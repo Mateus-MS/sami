9c4a6fb83580ed58274c2b83523fcfb996ce4557
Mateus-MS <alves.mateus1050@gmail.com>
2024-02-09 13:02:08 +0000

    foother v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { BannerComponent } from './components/banner/banner.component';
 import { CardComponent } from './components/card/card.component';
 import { CollectionComponent } from './components/collection/collection.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, BannerComponent, CardComponent, CollectionComponent, DropdownComponent],
+  imports: [NavbarComponent, BannerComponent, CardComponent, CollectionComponent, DropdownComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
