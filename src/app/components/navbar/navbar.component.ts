b38e2106e6744c856e864fc8097b2b7eace32e1f
Mateus-MS <alves.mateus1050@gmail.com>
2024-02-10 21:12:18 +0000

    bar transition test

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
@@ -8,6 +8,34 @@ import { LogoComponent } from '../logo/logo.component';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent{
+
+  constructor(private el: ElementRef){}
+
+  ngOnInit(): void{
+
+    let top_section : Element  | null = document.getElementById('top')
+
+    if(top_section === undefined || top_section === null) return
+
+    let onScreen: number;
+
+    let observer = new IntersectionObserver(entries =>{
+
+      onScreen = entries[0].intersectionRatio
+
+      if(onScreen >= .5){
+        this.el.nativeElement.style.setProperty('--background-color', 'var(--black)')
+        this.el.nativeElement.style.setProperty('--items-color', 'var(--white)')
+      } else {
+        this.el.nativeElement.style.setProperty('--background-color', 'var(--white)')
+        this.el.nativeElement.style.setProperty('--items-color', 'var(--black)')
+      }
+
+    }, {threshold: .5})
+
+    observer.observe(top_section)
+
+  }
 
 }
